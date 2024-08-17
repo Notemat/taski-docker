13f43b7fe571a1d2d5edd3382eb7f62fc750f55f
Никита Козлов <kozlov.nik@icloud.com>
2024-08-17 09:58:55 +0300

    add test self

--- backend/api/test.py
+++ backend/api/test.py
@@ -7,7 +7,7 @@ from django.test import Client, TestCase
 class TaskiAPITestCase(TestCase):
 
     def setUp(self):
-        set.guest_client = Client()
+        self.guest_client = Client()
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
